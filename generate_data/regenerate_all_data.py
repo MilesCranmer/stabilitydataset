5e02d0f5456bc79ddd1be60853fb222d0c431dff
dtamayo <tamayo.daniel@gmail.com>
2019-03-08 20:52:20 -0500

    removed training data gen scripts moved to MLstability

--- generate_data/regenerate_all_data.py
+++
@@ -1,8 +0,0 @@
-import os
-from subprocess import call
-
-for root, dirs, files in os.walk("."):
-    if root == '.':
-        continue
-    call('python ' + root + '/generate_data.py', shell=True)
-
